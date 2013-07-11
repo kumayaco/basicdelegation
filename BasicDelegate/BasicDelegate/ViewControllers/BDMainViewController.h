e5f0a2fc7471273c396826b1714e159d7cecef23
Vincent <vince78718@gmail.com>
2013-07-11 14:55:08 +0800

    Add Basic View Controller.

---
+++ BasicDelegate/BasicDelegate/ViewControllers/BDMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  BDMainViewController.h
+//  BasicDelegate
+//
+//  Created by ANS MI0002 Vincent on 2013/07/11.
+//  Copyright (c) 2013年 Donutz Creativity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BDMainViewController : UIViewController
+
+@end
---
+++ BasicDelegate/BasicDelegate/ViewControllers/BDMainViewController.m
@@ -0,0 +1,13 @@
+//
+//  BDMainViewController.m
+//  BasicDelegate
+//
+//  Created by ANS MI0002 Vincent on 2013/07/11.
+//  Copyright (c) 2013年 Donutz Creativity. All rights reserved.
+//
+
+#import "BDMainViewController.h"
+
+@implementation BDMainViewController
+
+@end
