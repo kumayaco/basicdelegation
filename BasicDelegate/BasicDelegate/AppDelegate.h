feb73ad7997de330bab504705448f09842bec881
Vincent <vince78718@gmail.com>
2013-07-11 14:40:45 +0800

    First Commit.

---
+++ BasicDelegate/BasicDelegate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BasicDelegate
+//
+//  Created by ANS MI0002 Vincent on 2013/07/11.
+//  Copyright (c) 2013年 Donutz Creativity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BasicDelegate/BasicDelegate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BasicDelegate
+//
+//  Created by ANS MI0002 Vincent on 2013/07/11.
+//  Copyright (c) 2013年 Donutz Creativity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
